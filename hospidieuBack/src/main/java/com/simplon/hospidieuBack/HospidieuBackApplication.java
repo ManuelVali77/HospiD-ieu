027c8ec2c30f2f85f67d69d0b171273f2fcd0722
ManuelVali77 <manuelv@outlook.fr>
2023-02-21 10:30:04 +0100

    creation du projet backend de l'application hospidieu

---
+++ hospidieuBack/src/main/java/com/simplon/hospidieuBack/HospidieuBackApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.hospidieuBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospidieuBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospidieuBackApplication.class, args);
+	}
+
+}
---
+++ hospidieuBack/src/test/java/com/simplon/hospidieuBack/HospidieuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.hospidieuBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospidieuBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
