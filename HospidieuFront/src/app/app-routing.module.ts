1b6413a3a01ee5560f362a77a7e6dfe01d760d44
ManuelVali77 <manuelv@outlook.fr>
2023-02-22 11:25:06 +0100

    page de connexion static + header dynamique

--- HospidieuFront/src/app/app-routing.module.ts
+++ HospidieuFront/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: ConnexionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
