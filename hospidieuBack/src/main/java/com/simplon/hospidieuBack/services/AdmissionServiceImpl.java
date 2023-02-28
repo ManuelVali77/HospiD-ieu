39ea0ceb0c73c7a7dc33c9237c36fd948009c5e6
SiderealStorm <112557944+SiderealStorm@users.noreply.github.com>
2023-02-28 14:25:18 +0100

    Added monitoring (forgot task)

--- hospidieuBack/src/main/java/com/simplon/hospidieuBack/services/AdmissionServiceImpl.java
+++ hospidieuBack/src/main/java/com/simplon/hospidieuBack/services/AdmissionServiceImpl.java
@@ -1,23 +1,28 @@
 package com.simplon.hospidieuBack.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.simplon.hospidieuBack.model.Bed;
+import com.simplon.hospidieuBack.model.Monitoring;
 import com.simplon.hospidieuBack.model.Patient;
 import com.simplon.hospidieuBack.model.PatientInBedDto;
 import com.simplon.hospidieuBack.repository.BedRepository;
+import com.simplon.hospidieuBack.repository.MonitoringRepository;
 import com.simplon.hospidieuBack.repository.PatientRepository;
 
 @Service
 public class AdmissionServiceImpl implements AdmissionService {
-
+	
 	@Autowired
 	private BedRepository bedRepo;
 	@Autowired
 	private PatientRepository patientRepo;
+	@Autowired
+	private MonitoringRepository monitoringRepo;
 
 	@Override
 	public List<Bed> getEmptyBeds() {
@@ -42,5 +47,22 @@ public class AdmissionServiceImpl implements AdmissionService {
 		
 		bed.setPatient(patient);
 		bedRepo.save(bed);
+		
+		addInOutMonitoring(patientInBed, "Admission");
+	}
+	
+	public void addInOutMonitoring(PatientInBedDto patientDto, String condition) {
+		Monitoring monitoring = new Monitoring();
+		monitoring.setDate(new Date());
+		
+		Patient patient = patientRepo.findPatientsByIdPatient(patientDto.getIdPatient());
+		monitoring.setPatient(patient);
+		
+//		// TODO à ajouter quand on pourra récupérer l'utilisateur :
+//		monitoring.setUser(user);
+		
+		monitoring.setComment(condition + " du patient dans le service " + patientDto.getDepartment() + ", chambre " + patientDto.getRoom() + " lit " + patientDto.getBed());
+		
+		monitoringRepo.save(monitoring);
 	}
 }
