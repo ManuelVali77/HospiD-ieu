480b92bf9fb714016dcffd9110a48baceb1843ff
SiderealStorm <112557944+SiderealStorm@users.noreply.github.com>
2023-03-01 09:29:11 +0100

    commit before pulling dev

--- hospidieuBack/src/main/java/com/simplon/hospidieuBack/HospidieuBackApplication.java
+++ hospidieuBack/src/main/java/com/simplon/hospidieuBack/HospidieuBackApplication.java
@@ -1,11 +1,17 @@
 package com.simplon.hospidieuBack;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.simplon.hospidieuBack.repository.PatientRepository;
+
 @SpringBootApplication
 public class HospidieuBackApplication  implements CommandLineRunner {
+	
+	@Autowired
+	PatientRepository repo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HospidieuBackApplication.class, args);
@@ -14,6 +20,7 @@ public class HospidieuBackApplication  implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("=== Bienvenue sur HospiD'ieu ===");
+		System.out.println(repo.findPatientsByIdPatient(1));
 	}
 
 }
